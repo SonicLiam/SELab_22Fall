8dcf7f0ddf73e898d75ccdf8ca42527adbc7eb21
SonicLiam <191220099@smail.nju.edu.cn>
2022-11-17 20:13:45 +0800

    Beatify output messages.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include "include/judge.h"
 #include "include/output.h"
 #define EXECUTE_TIMES 3
+
+void red_print(string s){
+    cout << "\033[31m" << s << "\033[0m" << endl;
+}
+
 int main(int argc, char *argv[]) {
     if(argc != 2){
         std::cout << "Usage: " << argv[0] << " <input path>" << std::endl;
@@ -13,33 +18,26 @@ int main(int argc, char *argv[]) {
     InputDir inputDir(argv[1]);
     //Output the scanned result
     auto programDirs = inputDir.getProgramDirs();
-    for(auto &programDir : programDirs){
-        int programNum = programDir.getProgramNum();
-        std::cout << "ProgramDir: " << programDir.getDir() << std::endl;
-        auto fileNames = programDir.getFileNames();
-        std::cout << "Got " << programNum << " programs:" << std::endl;
-        for(auto &fileName : fileNames){
-            std::cout  << fileName << std::endl;
-        }
-        std::cout << "Got format file:" << std::endl;
-        std::cout << programDir.getFormatFile() << std::endl;
-    }
+    red_print("Scanned " + to_string(programDirs.size()) + " program directories.");
 
     //Execute the programs
+    red_print("Executing programs:");
     for(auto &programDir : programDirs){
         Execute execute(&programDir);
         execute.execute(EXECUTE_TIMES);
     }
-    std::cout << "Executing completed. " << EXECUTE_TIMES << " times for each program." << std::endl;
+    red_print("Executing completed. " + to_string(EXECUTE_TIMES) + " times for each program.");
 
     //Judge the programs
     Judge judge(&inputDir, EXECUTE_TIMES);
     judge.judge();
     auto judgeResults = judge.getJudgeResults();
-    std::cout << "Judge completed. " << std::endl;
+    red_print("Judging completed. " + to_string(judgeResults.size()) + " pairs of programs are judged.");
 
     //Output the judge result
     Output output(&judge);
     output.output();
+    red_print("Output completed. The result is in the output directory.");
 
 }
+
